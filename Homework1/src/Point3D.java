f0025821db591b4c1fcbd2e70c07636376d4c937
Ilya <zakharuni13@gmail.com>
2014-09-30 14:39:40 +0400

    \\\
    clear
    git

--- Homework1/src/Point3D.java
+++ Homework1/src/Point3D.java
@@ -2,26 +2,46 @@
  * Created by Ilya on 27.09.2014.
  */
 public class Point3D extends Point {
-    private int x3;
-    private int y3;
+    private int z;
 
-    public int getX3() {
-        return x3;
+    public Point3D() {
+
     }
 
-    public int getY3() {
-        return y3;
+    public Point3D(int x, int y, int z) {
+        setX(x);
+        setY(y);
+        setZ(z);
     }
 
-    public void setX3(int x3) {
-        this.x3 = x3;
+    public int getZ() {
+        return z;
     }
 
-    public void setY3(int y3) {
-        this.y3 = y3;
+    public void setZ(int z) {
+        this.z = z;
     }
-    @Override
-    public double distance(Point3D p){
-        return 0;
+
+    public double distance(Point3D p) {
+        return Math.sqrt(Math.pow(p.getX() - this.getX(), 2) + Math.pow(p.getY() - this.getY(), 2) + Math.pow(p.getZ() - this.getZ(), 2));
+    }
+
+    public boolean oneLine(Point3D p1, Point3D p2) {
+        int x1 = this.getX();
+        int x2 = p1.getX();
+        int x3 = p2.getX();
+        int y1 = this.getY();
+        int y2 = p1.getY();
+        int y3 = p2.getY();
+        int z1 = this.getZ();
+        int z2 = p1.getZ();
+        int z3 = p2.getZ();
+
+        boolean first = (x3 - x1) * (y2 - y1) == (y3 - y1) * (x2 - x1) ? true : false;
+        boolean second = (z3 - z1)*(y2 - y1) == (y3 - y1)*(z2 - z1) ? true : false;
+
+        if(first & second)
+            return  true;
+        else return false;
     }
 }
--- Homework1/src/Program.java
+++ Homework1/src/Program.java
@@ -3,11 +3,17 @@
  */
 public class Program {
         public static void main(String[] args){
-            Point p1 = new Point(14,29);
-            Point p2 = new Point(22,43);
-            Point p3 = new Point(22,0);
+            Point p1 = new Point(2,0);
+            Point p2 = new Point(4,4);
+            Point p3 = new Point(5,6);
+            Point3D p1d = new Point3D(2, 2, 1);
+            Point3D p2d = new Point3D(2, 3, 4);
+            Point3D p3d = new Point3D(2, 1, -2);
+
             double result = p1.distance(p2);
             System.out.println(result);
-            System.out.println(p1.oneLine(p2,p3));
+            System.out.println(p1.oneLine(p2, p3));
+            System.out.println(p1d.oneLine(p2d,  p3d));
+            System.out.println(p1d.distance(p2d));
         }
     }
